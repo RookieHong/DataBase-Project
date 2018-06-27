3dbacfefa4806fc76d59b04adf9abcfd02d0948b
HongYigeng <120948741@qq.com>
2018-06-27 19:49:35 +0800

    完善了注册功能

--- DataBase Project/DataBase Project/register.cs
+++ DataBase Project/DataBase Project/register.cs
@@ -39,44 +39,82 @@ namespace DataBase_Project
             string connectionString = "server=localhost;database=Library;uid=sa;pwd=123456";
             SqlConnection con = new SqlConnection(connectionString);
 
+            string registerTran = String.Format(@"begin tran 
+                                    SET XACT_ABORT ON 
+                                    create login {0} with password='{1}', default_database=Library 
+                                    create user {2} for login {3} 
+                                    exec sp_addrolemember 'db_accessadmin', '{4}' 
+                                    exec sp_addrolemember 'admin', '{5}' 
+                                    insert into users values('{6}', '{7}', '{8}') 
+                                    commit tran", 
+                                    inputAccount, inputPassword, inputAccount, inputAccount, inputAccount, inputAccount, inputAccount, inputPassword, identity.Text);
             //string addLoginSql = "create login " + inputAccount + " with password='" + inputPassword + "', default_database=Library";
-            string addLoginSql = String.Format("create login {0} with password='{1}',default_database=Library",inputAccount,inputPassword);
+            //string addLoginSql = String.Format("create login {0} with password='{1}',default_database=Library",inputAccount,inputPassword);
             //string addUserSql = "EXEC sp_grantdbaccess '" + inputAccount + "','" + inputAccount + "'";
-            string addUserSql = String.Format("EXEC sp_grantdbaccess '{0}','{1}'", inputAccount, inputAccount);
-            string addRoleSql = "EXEC sp_addrolemember ";
-            if (inputIdentity == 0) addRoleSql += "'admin'";
-            else addRoleSql += "'reader'";
-            addRoleSql = addRoleSql + ",'" + inputAccount + "'";
-            string insertUserSql = String.Format("insert into users values('{0}','{1}','{2}')", inputAccount, inputPassword, identity.Text);
+            //string addUserSql = String.Format("EXEC sp_grantdbaccess '{0}','{1}'", inputAccount, inputAccount);
+            //string addRoleSql = "EXEC sp_addrolemember ";
+            //if (inputIdentity == 0) addRoleSql += "'admin'";
+            //else addRoleSql += "'reader'";
+            //addRoleSql = addRoleSql + ",'" + inputAccount + "'";
+            
 
-            SqlCommand addLoginCmd = new SqlCommand(addLoginSql, con);
-            SqlCommand addUserCmd = new SqlCommand(addUserSql, con);
-            SqlCommand addRoleCmd = new SqlCommand(addRoleSql, con);
-            SqlCommand insertUserCmd = new SqlCommand(insertUserSql, con);
+            SqlCommand registerCmd = new SqlCommand(registerTran, con);
+            //SqlCommand addLoginCmd = new SqlCommand(addLoginSql, con);
+            //SqlCommand addUserCmd = new SqlCommand(addUserSql, con);
+            //SqlCommand addRoleCmd = new SqlCommand(addRoleSql, con);
 
-            string result = "注册成功！";
+            //string result = "注册成功！";
 
             try
             {
                 con.Open();
-                addLoginCmd.ExecuteNonQuery();
-                addUserCmd.ExecuteNonQuery();
-                addRoleCmd.ExecuteNonQuery();
-                insertUserCmd.ExecuteNonQuery();
+                registerCmd.ExecuteNonQuery();
+                //addLoginCmd.ExecuteNonQuery();
+                //addUserCmd.ExecuteNonQuery();
+                //addRoleCmd.ExecuteNonQuery();
+                //insertUserCmd.ExecuteNonQuery();
             }
             catch (SqlException exception)
             {
                 //throw new Exception(exception.Message);
-                result = exception.Message;
+                if (MessageBox.Show(exception.Message, "错误") == DialogResult.OK)
+                {
+                    con.Close();
+                    registerCmd.Dispose();
+                    return;
+                }
             }
             finally
             {
-                addLoginCmd.Dispose();//对SqlCommand进行处理，回收
-                addUserCmd.Dispose();
-                addRoleCmd.Dispose();
-                insertUserCmd.Dispose();
-                con.Close();//连接关闭
-                if (MessageBox.Show(result, "提示") == DialogResult.OK) this.Close();
+                registerCmd.Dispose(); //对SqlCommand进行处理，回收
+                //addLoginCmd.Dispose();
+                //addUserCmd.Dispose();
+                //addRoleCmd.Dispose();
+                //insertUserCmd.Dispose();
+                //con.Close();//连接关闭
+                //if (MessageBox.Show(result, "提示") == DialogResult.OK) this.Close();
+            }
+
+            string addSrvRole = String.Format("exec sp_addsrvrolemember '{0}', 'securityadmin'", inputAccount);
+            SqlCommand addSrvRoleCmd = new SqlCommand(addSrvRole, con);
+            try
+            {
+                addSrvRoleCmd.ExecuteNonQuery();
+            }
+            catch(SqlException exception)
+            {
+                if (MessageBox.Show(exception.Message, "错误") == DialogResult.OK)
+                {
+                    con.Close();
+                    addSrvRoleCmd.Dispose();
+                    return;
+                }
+            }
+
+            if (MessageBox.Show("注册成功！", "提示") == DialogResult.OK)
+            {
+                con.Close();
+                addSrvRoleCmd.Dispose();
             }
         }
     }
